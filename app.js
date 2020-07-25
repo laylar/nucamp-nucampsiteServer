eb697f5a7898bd3644ee22de6653b4bff973d454
Layla <layla.rowen@gmail.com>
2020-07-25 15:09:30 -0700

    Week 4 Workshop Task 2 '/favorites' GET and POST

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-const passport = require('passport');
-const config = require('./config');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+const passport = require("passport");
+const config = require("./config");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const campsiteRouter = require('./routes/campsiteRouter');
-const partnerRouter = require('./routes/partnerRouter');
-const promotionRouter = require('./routes/promotionRouter');
-const uploadRouter = require('./routes/uploadRouter');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const campsiteRouter = require("./routes/campsiteRouter");
+const partnerRouter = require("./routes/partnerRouter");
+const promotionRouter = require("./routes/promotionRouter");
+const uploadRouter = require("./routes/uploadRouter");
+const favoriteRouter = require("./routes/favoriteRouter");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 //err here is the second argument in the anonymous callback function in the then() function.
 //It's an alternative to .catch if there won't be any other .then's chained on
 //.catch() is more common, but this way is possible, too.
-connect.then(() => console.log('Connected correctly to server'),
-  err => console.log(err)
+connect.then(
+  () => console.log("Connected correctly to server"),
+  (err) => console.log(err)
 );
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -44,15 +46,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter); //users should be able to access this before auth so they can create a new account
+app.use("/", indexRouter);
+app.use("/users", usersRouter); //users should be able to access this before auth so they can create a new account
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/campsites', campsiteRouter);
-app.use('/partners', partnerRouter);
-app.use('/promotions', promotionRouter);
-app.use('/imageUpload', uploadRouter);
+app.use("/campsites", campsiteRouter);
+app.use("/partners", partnerRouter);
+app.use("/promotions", promotionRouter);
+app.use("/imageUpload", uploadRouter);
+app.use("/favorites", favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -63,19 +66,24 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   if (req.secure) {
     return next();
   } else {
-    console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
-    res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    console.log(
+      `Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+    res.redirect(
+      301,
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
   }
 });
 
---
+++ routes/favoriteRouter.js
@@ -0,0 +1,137 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Favorite = require("../models/favorites");
+const authenticate = require("../authenticate");
+const cors = require("./cors");
+
+const favoriteRouter = express.Router();
+
+favoriteRouter.use(bodyParser.json());
+
+favoriteRouter
+  .route("/")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOne({ user: req.user._id })
+      .then((favorites) => {
+        if (favorites) {
+          Favorite.findById(favorites._id)
+            .populate("campsites")
+            .then((favorite) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json({ campsites: favorite.campsites });
+            });
+        } else {
+          err = new Error(`Favorites not found.`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOne({ user: req.user._id })
+      .then((favorites) => {
+        if (favorites) {
+          /** User's list of favorites exists but doesn't include the specified campsite */
+          req.body.forEach((faves) => {
+            if (!favorites.campsites.includes(faves._id)) {
+              favorites.campsites.push(faves._id);
+            }
+          });
+          favorites
+            .save()
+            .then((favorite) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(favorite);
+            })
+            .catch((err) => next(err));
+        } else {
+          /** User's list of favorites doesn't exist yet! */
+          Favorite.create({ user: req.user._id })
+            .then((favorites) => {
+              for (let i = 0; i < req.body.length; i++) {
+                if (!favorites.campsites.includes(req.body[i]._id)) {
+                  favorites.campsites.push(req.body[i]);
+                }
+              }
+              favorites
+                .save()
+                .then((favorites) => {
+                  Favorite.findById(favorites._id)
+                    .populate("user")
+                    .populate("campsites")
+                    .then((favorites) => {
+                      res.statusCode = 200;
+                      res.setHeader("Content-Type", "application/json");
+                      res.json(favorites);
+                    });
+                })
+                .catch((err) => next(err));
+            })
+            .catch((err) => next(err));
+        }
+      })
+      .catch((err) => next(err));
+  })
+
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /favorites");
+  })
+  .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.deleteMany()
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
+  });
+
+favoriteRouter
+  .route("/:campsiteId")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findById(req.params.favoriteId)
+      .then((favorite) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(favorite);
+      })
+      .catch((err) => next(err));
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation not supported on /favorites/${req.params.favoriteId}`
+    );
+  })
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findByIdAndUpdate(
+      req.params.favoriteId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    )
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
+  })
+  .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findByIdAndDelete(req.params.favoriteId)
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
+  });
+
+module.exports = favoriteRouter;
